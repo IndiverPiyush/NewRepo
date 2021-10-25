cb7289afef9de8f8805f45d876505e62d8ae84e8
Atishay Vishwakarma <58907@hexaware.com>
2021-10-25 12:10:15 +0530

    Push01

---
+++ MedicineSearchWebApp/MedicineSearchWebApp/Models/Medecine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MedicineSearchWebApp.Models
+{
+    public partial class Medecine
+    {
+        public int MedicineId { get; set; }
+        public int ProviderId { get; set; }
+        public string MedicineName { get; set; }
+        public string MedicineCategory { get; set; }
+        public int MedicineDosage { get; set; }
+        public DateTime MedicineMdate { get; set; }
+        public DateTime MedicineEdate { get; set; }
+        public int MedicineStock { get; set; }
+        public decimal MedicinePrice { get; set; }
+
+        public virtual Vendor Provider { get; set; }
+    }
+}
---
+++ MedicineSearchWebApp/MedicineSearchWebApp/Models/Vendor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MedicineSearchWebApp.Models
+{
+    public partial class Vendor
+    {
+        public Vendor()
+        {
+            Medecines = new HashSet<Medecine>();
+        }
+
+        public int VendorId { get; set; }
+        public string VendorOrgName { get; set; }
+        public string VendorArea { get; set; }
+        public string VendorCity { get; set; }
+        public string VendorMobile { get; set; }
+        public decimal VendorWallet { get; set; }
+        public string VendorPassword { get; set; }
+        public string VendorSpeciality { get; set; }
+
+        public virtual ICollection<Medecine> Medecines { get; set; }
+    }
+}
